b0ff686eec8f53304b4ec9a1b5536d3859207a40
ithevbro <ithevbro@gmail.com>
2024-05-07 15:33:21 +0300

    css pizza

--- src/components/Categories/Pizza/Pizza.jsx
+++ src/components/Categories/Pizza/Pizza.jsx
@@ -2,14 +2,32 @@ import { getProds } from "../../../service/services"
 import style from './pizza.module.css'
 import PizzaElement from "./PizzaElement"
 import LoaderMain from "../../Loaders/Loader-main"
+import { useState } from "react"
+import SelectFilter from "../../SelectFilter/SelectFilter"
 
 function Pizza() {
     let { products, loading } = getProds('pizza')
+    const [prodFilter, setProdFilter] = useState('Всі')
+    const [sizeFilter, setSizeFilter] = useState(22)
 
     return (
         <section className={style.section_wrapper}>
             <h1>Піца у Львові</h1>
-            <aside></aside>
+            <aside>
+                <ul className={style.prod_filter}>
+                    <li onClick={() => setProdFilter('Всі')} className={prodFilter === 'Всі' ? style.sushi_filter_list_active_color : ''}>Всі</li>
+                    <li onClick={() => setProdFilter('ТОП продажів')} className={prodFilter === 'ТОП продажів' ? style.sushi_filter_list_active_color : ''}>ТОП продажів</li>
+                    <li onClick={() => setProdFilter("М'ясні")} className={prodFilter === "М'ясні" ? style.sushi_filter_list_active_color : ''}>М'ясні</li>
+                    <li onClick={() => setProdFilter("Вегетаріанські")} className={prodFilter === "Вегетаріанські" ? style.sushi_filter_list_active_color : ''}>Вегетаріанські</li>
+                    <li onClick={() => setProdFilter("Гострі")} className={prodFilter === "Гострі" ? style.sushi_filter_list_active_color : ''}>Гострі</li>
+                </ul>
+                <ul className={style.prod_filter}>
+                    <li onClick={() => setSizeFilter(22)} className={prodFilter === 22 ? style.sushi_filter_list_active_color : ''}>22</li>
+                    <li onClick={() => setSizeFilter('ТОП продажів')} className={prodFilter === 'ТОП продажів' ? style.sushi_filter_list_active_color : ''}>ТОП продажів</li>
+                    <li onClick={() => setSizeFilter("М'ясні")} className={prodFilter === "М'ясні" ? style.sushi_filter_list_active_color : ''}>М'ясні</li>
+                </ul>
+                <SelectFilter />
+            </aside>
             {loading ? <LoaderMain /> :
                 <ul className={style.prod_list}>
                     {products.map(item => (
--- src/components/Categories/Pizza/PizzaElement.jsx
+++ src/components/Categories/Pizza/PizzaElement.jsx
@@ -3,30 +3,41 @@ import { useState } from "react"
 function PizzaElement({ item, style }) {
 
     const [pizzaSize, setPizzaSize] = useState(22)
+    const [bortik, setBortik] = useState(false)
 
     function currentSize(size) {
-        setPizzaSize(size);
+        setBortik(false)
+        setPizzaSize(size)
+    }
+
+    function toggleBortik() {
+        setBortik(!bortik)
     }
 
     return (
         <li className={style.pizza_element}>
             <img src={item.image} alt="" />
             <div className={style.product_description}>
-                <p className={style.weight}>{item.weight}</p>
+                <p className={style.weight}>{item.weight} Г</p>
                 <p><b>{item.title}</b></p>
                 <div className={style.product_controls}>
                     <p><b>{item.price} грн</b></p>
-                    <button>КУПИТИ</button>
+                    <button className="prod_buy_btn">КУПИТИ</button>
+                    {/* <div className="minus_plus">
+                        <button>-</button>
+                        <span>1</span>
+                        <button>+</button>
+                    </div> */}
                 </div>
                 <ul className={style.pizza_size}>
                     <li id={pizzaSize === 22 ? style.current_pizza_size : ''} onClick={() => currentSize(22)}>22 см</li>
                     <li id={pizzaSize === 30 ? style.current_pizza_size : ''} onClick={() => currentSize(30)}>30 см</li>
                     <li id={pizzaSize === 40 ? style.current_pizza_size : ''} onClick={() => currentSize(40)}>40 см</li>
                 </ul>
-                {pizzaSize > 22 ? <button className={style.bortik}>Додати сирний бортик</button> : null}
+                {pizzaSize > 22 ? <button onClick={toggleBortik} className={`${style.bortik} ${bortik ? style.bortiktrue : ''}`}>{bortik ? 'Видалити сирний бортик' : 'Додати сирний бортик'}</button> : null}
                 <p>{item.desription}</p>
             </div>
-        </li>
+        </li >
     )
 }
 
