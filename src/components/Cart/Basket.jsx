1e75b734ca47a5fa7133736956e2b543dbab4315
ithevbro <ithevbro@gmail.com>
2024-05-09 13:42:48 +0300

    basket-scroll-fix

--- src/components/Cart/Basket.jsx
+++ src/components/Cart/Basket.jsx
@@ -23,59 +23,61 @@ function Basket() {
 
 
     return (
-        <div className={style.basket_container}>
-            <h5 className={style.basket_title}>Кошик</h5>
-            <form>
-                {dataCart.cartData && dataCart.cartData?.map((item) => {
-                    return (
-                        <div key={item._id} className={style.basket_element_in_list}>
-                            <div className={style.basket_element_in_list_image}><img src={item.image} alt="" /></div>
-                            <div className={style.basket_element_info}>
-                                <div className={style.basket_element_weight}>{item.weight} г</div>
-                                <div className={style.basket_element_title}>{item.title}</div>
-                            </div>
-                            <div className={style.bottom_info}>
-                                <div className={style.btn_cont}>
-                                    <button className={style.plus_minus_btn} id={style.btn} onClick={(e) => {
-                                        e.preventDefault()
-                                        plusElement(item)
-                                    }}>
-                                        +
-                                    </button>
-                                    <span className={style.item_quantity}>{item.q}</span>
-                                    <button id={style.btn} onClick={(e) => {
-                                        e.preventDefault()
-                                        minusElement(item)
-                                    }}>
-                                        -
-                                    </button>
+        <div className={style.bs2}>
+            <div className={style.basket_container}>
+                <h5 className={style.basket_title}>Кошик</h5>
+                <form>
+                    {dataCart.cartData && dataCart.cartData?.map((item) => {
+                        return (
+                            <div key={item._id} className={style.basket_element_in_list}>
+                                <div className={style.basket_element_in_list_image}><img src={item.image} alt="" /></div>
+                                <div className={style.basket_element_info}>
+                                    <div className={style.basket_element_weight}>{item.weight} г</div>
+                                    <div className={style.basket_element_title}>{item.title}</div>
                                 </div>
-                                <div className={style.prive_quant}>{item.price * item.q}<span> грн</span></div>
+                                <div className={style.bottom_info}>
+                                    <div className={style.btn_cont}>
+                                        <button className={style.plus_minus_btn} id={style.btn} onClick={(e) => {
+                                            e.preventDefault()
+                                            plusElement(item)
+                                        }}>
+                                            +
+                                        </button>
+                                        <span className={style.item_quantity}>{item.q}</span>
+                                        <button id={style.btn} onClick={(e) => {
+                                            e.preventDefault()
+                                            minusElement(item)
+                                        }}>
+                                            -
+                                        </button>
+                                    </div>
+                                    <div className={style.prive_quant}>{item.price * item.q}<span> грн</span></div>
+                                </div>
+                                <div className={style.btn_delete_from_cart} onClick={(e) => {
+                                    e.preventDefault()
+                                    removeFromCart(item)
+                                }}></div>
                             </div>
-                            <div className={style.btn_delete_from_cart} onClick={(e) => {
-                                e.preventDefault()
-                                removeFromCart(item)
-                            }}></div>
+                        )
+                    })}
+                    <hr className={style.border_bottom_basket} />
+                    <div className={style.basket_summary}>
+                        <div className={style.basket_payment}>
+                            <div>До оплати:</div>
+                            <div>{sum()} грн</div>
                         </div>
-                    )
-                })}
-                <hr className={style.border_bottom_basket} />
-                <div className={style.basket_summary}>
-                    <div className={style.basket_payment}>
-                        <div>До оплати:</div>
-                        <div>{sum()} грн</div>
+                        <div className={style.delivery_text}>Сума мінімального замовлення в Зеленій зоні - 400 грн, у Жовтій зоні - 700 грн. В суму мінімального замовлення не входять напої</div>
+                        {
+                            dataCart.cartData.length > 0 ? <NavLink to={'/order'} onClick={() => setGlobalOverlayData({ type: 'main' })}><button className={style.basket_buy_button}>ОФОРМИТИ ЗАМОВЛЕННЯ</button></NavLink> : <button className={style.basket_buy_button_disabled} disabled>ОФОРМИТИ ЗАМОВЛЕННЯ</button>
+                        }
                     </div>
-                    <div className={style.delivery_text}>Сума мінімального замовлення в Зеленій зоні - 400 грн, у Жовтій зоні - 700 грн. В суму мінімального замовлення не входять напої</div>
-                    {
-                        dataCart.cartData.length > 0 ? <NavLink to={'/order'} onClick={() => setGlobalOverlayData({ type: 'main' })}><button className={style.basket_buy_button}>ОФОРМИТИ ЗАМОВЛЕННЯ</button></NavLink> : <button className={style.basket_buy_button_disabled} disabled>ОФОРМИТИ ЗАМОВЛЕННЯ</button>
-                    }
-                </div>
-            </form>
-            <a className={style.enter_in_account} onClick={(e) => {
-                e.preventDefault()
-                setGlobalOverlayData({ type: 'login' })
-            }} href="">Увійти в особистий кабінет</a>
-            <div className={style.close_busket_btn} onClick={() => setGlobalOverlayData({ type: 'main' })}></div>
+                </form>
+                <a className={style.enter_in_account} onClick={(e) => {
+                    e.preventDefault()
+                    setGlobalOverlayData({ type: 'login' })
+                }} href="">Увійти в особистий кабінет</a>
+                <div className={style.close_busket_btn} onClick={() => setGlobalOverlayData({ type: 'main' })}></div>
+            </div>
         </div>
     )
 }
